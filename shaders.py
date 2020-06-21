3649794d44c7e38bc267756b78f403939382c75e
Shawn Presser <shawnpresser@gmail.com>
2020-06-21 13:34:44 -0700

    Cleanup

--- shaders.py
+++ shaders.py
@@ -24,7 +24,6 @@ class TexturedLitShader(renderer.Shader):
         default_diffuse = np.array([1, 1, 1], dtype=np.float32)
         default_wvp = np.eye(4, dtype=np.float32)
         default_world = np.eye(4, dtype=np.float32)
-        default_inv_world = np.eye(4, dtype=np.float32)
         default_norm = np.eye(3, dtype=np.float32)
 
         self.light_dir = tf.placeholder_with_default(default_light_dir, [3])
@@ -32,12 +31,10 @@ class TexturedLitShader(renderer.Shader):
         self.diffuse = tf.placeholder_with_default(default_diffuse, [3])
         self.wvp = tf.placeholder_with_default(default_wvp, [4, 4])
         self.world = tf.placeholder_with_default(default_world, [4, 4])
-        #self.inv_world = tf.placeholder_with_default(default_inv_world, [4, 4])
-        self.inv_world = tf.matrix_inverse(self.world)
+        #self.inv_world = tf.matrix_inverse(self.world)
         self.norm = tf.placeholder_with_default(default_norm, [3, 3])
 
         self.packed_texture = utils.pack_colors(self.texture, 2, False)
-        #self.iwvp = tf.matrix_inverse(self.wvp)
 
         self.varying_pos = [None, None, None]
         self.varying_uv = [None, None, None]
@@ -53,84 +50,36 @@ class TexturedLitShader(renderer.Shader):
         vertices = tf.gather(self.vertices, indices)
         normals = tf.gather(self.normals, indices)
         uvs = tf.gather(self.uvs, indices)
-        #import pdb; pdb.set_trace()
         vertices = tf.concat([vertices, tf.ones([num_verts, 1])], 1)
         normals = tf.concat([normals, tf.zeros([num_verts, 1])], 1)
-        #self.varying_uv[vertex_id] = uvs
-        #self.varying_norm[vertex_id] = tf.matmul(
-        #    normals, self.iwvp, transpose_b=True)[:, :3]
-        result = tf.matmul(vertices, self.wvp, transpose_b=True)
-        # w = result[:, 3]
-        # u_w = uvs[:, 0] / w
-        # v_w = uvs[:, 1] / w
-        # z_w = result[:, 2] / w
-        # i_w = 1.0 / w
-        # texcoords = tf.stack([u_w, v_w, z_w, i_w], 1)
-        # self.varying_tc[vertex_id] = texcoords
-        w = result[:, 3]
-        x = result[:, 0] / w
-        y = result[:, 1] / w
-        z = result[:, 2] / w
-        self.varying_pos[vertex_id] = tf.stack([x, y, z, 1.0 / w], 1)
-        #self.varying_uv[vertex_id] /= w[vertex_id]
-        #self.varying_norm[vertex_id] /= w[vertex_id]
+        pos = tf.matmul(vertices, self.wvp, transpose_b=True)
+        x = pos[:, 0]
+        y = pos[:, 1]
+        z = pos[:, 2]
+        w = pos[:, 3]
+        self.varying_pos[vertex_id] = tf.stack([x/w, y/w, z/w, 1.0/w], 1)
         self.varying_uv[vertex_id] = self.output(w, uvs, 2)
         norm = tf.matmul(normals, self.world, transpose_b=True)[:, :3]
         self.varying_norm[vertex_id] = self.output(w, norm, 3)
-        return result
+        return pos
 
     @utils.op_scope
     def varying(self, w, v, bc, i, k):
-        # pos = utils.tri_dot(utils.tri_gather(self.varying_pos, i), bc)
-        # w = 1.0 / pos[:, 3]
-        # #tc = utils.tri_dot(utils.tri_gather(v, i) / w, bc) * w
-        # #return tc
         tc = utils.tri_dot(utils.tri_gather(v, i), bc)
         tc *= tf.stack([w for _ in range(k)], 1)
         return tc
-        # assert k in [2, 3]
-        # if k == 2:
-        #   tc_u = tc[:, 0] * w
-        #   tc_v = tc[:, 1] * w
-        #   return tf.stack([tc_u, tc_v], 1)
-        # elif k == 3:
-        #   tc_x = tc[:, 0] * w
-        #   tc_y = tc[:, 1] * w
-        #   tc_z = tc[:, 2] * w
-        #   return tf.stack([tc_x, tc_y, tc_z], 1)
-        # # tc_u = tc[:, 0] * w
-        # # tc_v = tc[:, 1] * w
-        # # tc_z = tc[:, 2] * w
-        # # tc_uv = tf.stack([tc_u, tc_v], 1)
-        # # tex = utils.unpack_colors(utils.sample(self.packed_texture, tc_uv), 1)
-      
 
     @utils.op_scope
     def fragment(self, bc, i):
         num_frags = tf.shape(bc[0])[0]
-        #import pdb; pdb.set_trace()
         bc = utils.tri_expand_dims(bc, 1)
         pos = utils.tri_dot(utils.tri_gather(self.varying_pos, i), bc)
         w = 1.0 / pos[:, 3]
-        #norm = utils.tri_dot(utils.tri_gather(self.varying_norm, i), bc)
         norm = self.varying(w, self.varying_norm, bc, i, 3)
         norm = tf.nn.l2_normalize(norm, 1)
         l = tf.expand_dims(tf.nn.l2_normalize(self.light_dir, 0), 1)
         d = utils.clamp(tf.matmul(norm, l), 0., 1.)
         uv = self.varying(w, self.varying_uv, bc, i, 2)
-        #uv = utils.tri_dot(utils.tri_gather(self.varying_uv, i), bc)
-        #tc = utils.tri_dot(utils.tri_gather(self.varying_tc, i), bc)
-        #import pdb; pdb.set_trace()
-        # tc_w = 1.0 / tc[:, 3]
-        # tc_u = tc[:, 0] * tc_w
-        # tc_v = tc[:, 1] * tc_w
-        # tc_z = tc[:, 2] * tc_w
-        # tc_uv = tf.stack([tc_u, tc_v], 1)
-        # tex = utils.unpack_colors(utils.sample(self.packed_texture, tc_uv), 1)
-        #tex = utils.unpack_colors(utils.sample(self.packed_texture, uv), 1)
         tex = utils.sample(self.packed_texture, uv)
         result = (self.ambient + self.diffuse * d) * tex
-        #result = (self.ambient + self.diffuse * d)
-        #import pdb; pdb.set_trace()
-        #result = tf.broadcast_to([uv[0], uv[1], 0.0])
         return result
